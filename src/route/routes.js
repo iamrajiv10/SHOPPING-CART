2797021a83b5b6b19f0ddc7942a779270ac99cb0
Ashishtripathi29 <ashishtripathipara@gmail.com>
2022-10-10 23:14:13 +0530

    start

--- src/route/routes.js
+++ src/route/routes.js
@@ -1,7 +1,10 @@
 const express=require("express")
+const usercontroller=require("../controller/userController")
 
 const router = express.Router();
 
+
+router.post("/register", usercontroller.createUser)
 router.get("/demo/:name",function(req,res){
     res.send(req.params.name)
 })
